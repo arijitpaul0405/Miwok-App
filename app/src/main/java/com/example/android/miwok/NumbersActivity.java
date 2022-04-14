e9d77f0e70a3b0bc8acb9516385af1a2eb00e366
Arijit Paul <arijitpaul541@gmail.com>
2022-04-14 23:11:27 +0530

    Updates:- 1. Upgraded Gradle version to 7.2 2. Upgraded AGP dependency from 7.0.4 to 7.1.3

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.miwok;
 
 import android.content.Context;
@@ -103,6 +88,7 @@ public class NumbersActivity extends AppCompatActivity {
                     AudioManager.STREAM_MUSIC,
                     // Request temporary focus for short period of time.
                     AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
             if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
                 //Setting audio to the MediaPlayer Object
                 mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getmAudioResourceId());
